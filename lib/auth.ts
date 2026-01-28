3d1029e1fef45bd74cf25ddbd2c2980406233ecd
Blas Vernazza <blasvernazza123@gmail.com>
2026-01-28 09:00:03 -0300

    fix: UploadThing setup, user schema setup

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,19 @@
-import {betterAuth} from "better-auth";
-import {drizzleAdapter} from "better-auth/adapters/drizzle";
-import {db} from ""
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/app/db/drizzle";
+import { schema } from "@/app/db/schema";
+import { nextCookies } from "better-auth/next-js";
+
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true,
+    },
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema
+    }),
+    plugins: [
+      nextCookies()   
+    ],
+});
